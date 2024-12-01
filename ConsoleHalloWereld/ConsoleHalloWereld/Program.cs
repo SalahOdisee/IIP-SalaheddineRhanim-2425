42c27128cade89f64428ac9e8e776df7c3526224
SalahOdisee <salaheddine.rhanim@gmail.com>
2024-12-01 20:52:55 +0100

    Taak ConsoleHalloWereld klaar !

---
+++ ConsoleHalloWereld/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleHalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hoe heet je:");
+            string naam = Console.ReadLine();
+            Console.WriteLine($"Aangename kennismaking, {(naam == "Salah eddine" ? "Salah eddine" : "onbekend")}");
+            Console.WriteLine("druk op een toest om verder te gaan...");
+            Console.ReadKey();
+
+        }
+    }
+}

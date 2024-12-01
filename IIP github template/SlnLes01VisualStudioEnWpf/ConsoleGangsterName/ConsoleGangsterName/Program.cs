9d8b59351f557b489b63148fc11307a1c2a947a0
SalahOdisee <salaheddine.rhanim@gmail.com>
2024-12-01 23:53:19 +0100

    Taak ConsoleGangsterName Klaar !

---
+++ IIP github template/SlnLes01VisualStudioEnWpf/ConsoleGangsterName/ConsoleGangsterName/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleGangsterName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string gangstaNameBuilder = @"
+***********************
+| GANGSTA NAME BUILDER |
+***********************
+";
+            Console.WriteLine(gangstaNameBuilder);
+            Console.Write("Give the first name of any Disney character: ");
+            string name = Console.ReadLine();
+            Console.Write("Give any workbench tool:  ");
+            string tool = Console.ReadLine();
+            Console.Write("What is your last name: ");
+            string LastName = Console.ReadLine();
+
+            string gangstaName = $"{name} 'the {tool}' {LastName}";
+            Console.Write("Your gangsta name : ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(gangstaName);
+            
+            Console.ResetColor();
+        }
+        
+    }
+}
